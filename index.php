c1a07d9a01df3a9e4e143118a6c68693818e5ed5
Jaron Bloom <j-bloom7@protonmail.com>
2024-08-13 23:25:26 -0400

    Changed index page and setup db query

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+$dsn = "mysql:host=localhost;port=3306;dbname=script_saver;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM scripts");
+$statement->execute();
+
+$scripts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// echo '<pre>';
+// var_dump($scripts);
+// echo '</pre>';
+// 
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -10,67 +26,39 @@
 
 </head>
 
-<body class="p-5">
-    <h1>Scripts</h1>
+<body>
+    <div class="p-5">
+        <h1>Scripts</h1>
 
-    <form method="POST" action="index.php">
-        <div class="mb-3 mt-4">
-            <div class="d-flex justify-content-center">
-                <div class="mb-3 ms-5">
-                    <label for="languageSelect" class="form-label">Language</label>
-                    <select id="languageSelect" class="form-select">
-                        <option disabled selected value> -- select an option -- </option>
-                        <option value="bash">Bash</option>
-                        <option value="css">CSS</option>
-                        <option value="html">HTML</option>
-                        <option value="javascript">JavaScript</option>
-                        <option value="php">PHP</option>
-                        <option value="python">Python</option>
-                        <option value="typescript">Typscript</option>
-                    </select>
-                </div>
-                <div class="mb-3 ms-5">
-                    <label for="developmentSelect" class="form-label">Development Type</label>
-                    <select id="developmentSelect" class="form-select">
-                        <option disabled selected value> -- select an option -- </option>
-                        <option value="api">API</option>
-                        <option value="backend">Backend</option>
-                        <option value="frontend">Frontend</option>
-                        <option value="system">System</option>
-                        <option value="system">SQL</option>
-                    </select>
-                </div>
-                <div class="mb-3 ms-5">
-                    <label for="taskSelect" class="form-label">Task Type</label>
-                    <select id="taskSelect" class="form-select">
-                        <option disabled selected value> -- select an option -- </option>
-                        <option value="automation">Automation</option>
-                        <option value="color">Colour</option>
-                        <option value="fontText">Font/Text</option>
-                        <option value="queries">Queries</option>
-                        <option value="cte">CTEs</option>
-                        <option value="storedprocedure">Stored Procedures</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <div class="d-flex justify-content-center">
-            <div class="border w-50 p-3">
-                <div class="mx-5">
-                    <label for="title" class="form-label">Description</label>
-                    <input type="text" class="form-control" id="title" aria-describedby="emailHelp">
-                    <div class="mb-3">
-                        <label for="script" class="form-label">Script</label>
-                        <textarea type="text" class="form-control" rows="10" style="height:100%;"
-                            id="script"></textarea>
-                    </div>
-                    <div class="d-grid gap-2 d-md-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
+        <a href="/create.php" class="btn btn-secondary mt-2 mb-2">Create new Script</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">What the scripte does</th>
+                    <th scope="col">Language</th>
+                    <th scope="col">Affected Area</th>
+                    <th scope="col">Task</th>
+                    <th scope="col">See More</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($scripts as $i => $script) : ?>
+                <tr>
+                    <th scope="row"><?= $i + 1 ?></th>
+                    <td><?= htmlspecialchars($script['description']) ?></td>
+                    <td><?= htmlspecialchars($script['language']) ?></td>
+                    <td><?= htmlspecialchars($script['development_type']) ?></td>
+                    <td><?= htmlspecialchars($script['task_type']) ?></td>
+                    <td>
+                        <a href="#" class="btn btn-success">View</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
