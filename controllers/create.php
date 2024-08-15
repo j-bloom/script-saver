19b2d95274757fc9f25db5842249b25dca50a331
Jaron Bloom <j-bloom7@protonmail.com>
2024-08-15 15:26:42 -0400

    move controllers to controller file

---
+++ controllers/create.php
@@ -0,0 +1,27 @@
+<?php
+
+$dsn = "mysql:host=localhost;port=3306;dbname=script_saver;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $description = $_POST['description'] ?? '';
+    $script = $_POST['script'] ?? '';
+    $language = $_POST['languageSelect'] ?? '';
+    $development_type = $_POST['developmentSelect'] ?? '';
+    $task_type = $_POST['taskSelect'] ?? '';
+
+    $statement = $pdo->prepare("INSERT INTO scripts(description, script, language, development_type, task_type) 
+                            VALUES(:description, :script, :language, :development_type, :task_type)");
+
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':script', $script);
+    $statement->bindValue(':language', $language);
+    $statement->bindValue(':development_type', $development_type);
+    $statement->bindValue(':task_type', $task_type);
+
+    header('Location: index.php');
+    $statement->execute();
+}
+
+require('views/create.view.php');
---
+++ controllers/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$dsn = "mysql:host=localhost;port=3306;dbname=script_saver;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM scripts");
+$statement->execute();
+
+$scripts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+require('views/index.view.php');
