7f7b229c5efbfdb693374dc3d64c60ebf1d57cd7
Jaron Bloom <j-bloom7@protonmail.com>
2024-08-17 16:36:31 -0400

    create database class and restructure database calls

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'script_saver',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4'
+    ]
+];
--- controllers/index.php
+++ controllers/index.php
@@ -1,11 +1,8 @@
 <?php
 
-$dsn = "mysql:host=localhost;port=3306;dbname=script_saver;user=root;password=;charset=utf8mb4";
-$pdo = new PDO($dsn);
+$config = require('config.php');
 
-$statement = $pdo->prepare("SELECT * FROM scripts");
-$statement->execute();
-
-$scripts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database'], $config['database']['user'], $config['database']['password']);
+$scripts = $db->query("SELECT * FROM scripts")->fetchAll();
 
 require('views/index.view.php');
