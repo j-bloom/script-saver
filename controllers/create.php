2c2683a85fba2b35c8a173415b669e81cae9fdca
Jaron Bloom <j-bloom7@protonmail.com>
2024-08-24 02:10:58 -0400

    update create functionality for better performance and safety

--- controllers/create.php
+++ controllers/create.php
@@ -4,7 +4,31 @@ $config = require('config.php');
 
 $db = new Database($config['database'], $config['database']['user'], $config['database']['password']);
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+    if (!isset($_POST['languageSelect']) || empty(trim($_POST['languageSelect']))) {
+        $errors['languageSelect'] = 'Language is required';
+    }
+
+    if (!isset($_POST['developmentSelect']) || empty(trim($_POST['developmentSelect']))) {
+        $errors['developmentSelect'] = 'Development type is required';
+    }
+
+    if (!isset($_POST['taskSelect']) || empty(trim($_POST['taskSelect']))) {
+        $errors['taskSelect'] = 'Task type is required';
+    }
+
+    if (empty(trim($_POST['description']))) {
+        $errors['description'] = 'Description is required';
+    }
+
+    if (empty(trim($_POST['script']))) {
+        $errors['script'] = 'Script is required';
+    }
+
 
     $description = $_POST['description'] ?? '';
     $script = $_POST['script'] ?? '';
@@ -12,17 +36,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $development_type = $_POST['developmentSelect'] ?? '';
     $task_type = $_POST['taskSelect'] ?? '';
 
-    $statement = $db->query("INSERT INTO scripts(description, script, language, development_type, task_type) 
+    if (empty($errors)) {
+        $statement = $db->query("INSERT INTO scripts(description, script, language, development_type, task_type) 
                             VALUES(:description, :script, :language, :development_type, :task_type)");
 
-    $statement->bindValue(':description', $description);
-    $statement->bindValue(':script', $script);
-    $statement->bindValue(':language', $language);
-    $statement->bindValue(':development_type', $development_type);
-    $statement->bindValue(':task_type', $task_type);
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':script', $script);
+        $statement->bindValue(':language', $language);
+        $statement->bindValue(':development_type', $development_type);
+        $statement->bindValue(':task_type', $task_type);
 
-    header('Location: index.php');
-    $statement->execute();
+        header('Location: index.php');
+        $statement->execute();
+    }
 }
 
 require('views/create.view.php');
--- views/create.view.php
+++ views/create.view.php
@@ -2,57 +2,124 @@
 <?php require('partials/nav.php') ?>
 <div class="p-5">
     <h1>Scripts</h1>
-    <form method="POST" action="create.php">
+    <form method="POST" action="">
         <div class="mb-3 mt-4">
             <div class="d-flex justify-content-center">
                 <div class="mb-3 ms-5">
                     <label for="languageSelect" class="form-label">Language</label>
-                    <select id="languageSelect" name="languageSelect" class="form-select">
+                    <select id="languageSelect" name="languageSelect" class="form-select" required>
                         <option disabled selected value> -- select an option -- </option>
-                        <option value="Bash">Bash</option>
-                        <option value="CSS">CSS</option>
-                        <option value="HTML">HTML</option>
-                        <option value="Javascript">JavaScript</option>
-                        <option value="PHP">PHP</option>
-                        <option value="Python">Python</option>
-                        <option value="Typescript">Typscript</option>
+                        <option value="Bash"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'Bash') ? 'selected' : '' ?>>
+                            Bash</option>
+                        <option value="CSS"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'CSS') ? 'selected' : '' ?>>
+                            CSS</option>
+                        <option value="HTML"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'HTML') ? 'selected' : '' ?>>
+                            HTML</option>
+                        <option value="Javascript"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'Javascript') ? 'selected' : '' ?>>
+                            JavaScript</option>
+                        <option value="PHP"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'PHP') ? 'selected' : '' ?>>
+                            PHP</option>
+                        <option value="Python"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'Python') ? 'selected' : '' ?>>
+                            Python</option>
+                        <option value="Typescript"
+                            <?= (isset($_POST['languageSelect']) && $_POST['languageSelect'] == 'Typescript') ? 'selected' : '' ?>>
+                            Typescript</option>
                     </select>
+                    <?php if (isset($errors['languageSelect'])) : ?>
+                    <p class="text-danger"><?= $errors['languageSelect'] ?></p>
+                    <?php endif; ?>
+
                 </div>
+
                 <div class="mb-3 ms-5">
                     <label for="developmentSelect" class="form-label">Development Type</label>
-                    <select id="developmentSelect" name="developmentSelect" class="form-select">
+                    <select id="developmentSelect" name="developmentSelect" class="form-select" required>
                         <option disabled selected value> -- select an option -- </option>
-                        <option value="Api">API</option>
-                        <option value="Backend">Backend</option>
-                        <option value="Frontend">Frontend</option>
-                        <option value="System">System</option>
-                        <option value="SQL">SQL</option>
+                        <option value="Api"
+                            <?= (isset($_POST['developmentSelect']) && $_POST['developmentSelect'] == 'Api') ? 'selected' : '' ?>>
+                            API</option>
+                        <option value="Backend"
+                            <?= (isset($_POST['developmentSelect']) && $_POST['developmentSelect'] == 'Backend') ? 'selected' : '' ?>>
+                            Backend</option>
+                        <option value="Frontend"
+                            <?= (isset($_POST['developmentSelect']) && $_POST['developmentSelect'] == 'Frontend') ? 'selected' : '' ?>>
+                            Frontend</option>
+                        <option value="System"
+                            <?= (isset($_POST['developmentSelect']) && $_POST['developmentSelect'] == 'System') ? 'selected' : '' ?>>
+                            System</option>
+                        <option value="SQL"
+                            <?= (isset($_POST['developmentSelect']) && $_POST['developmentSelect'] == 'SQL') ? 'selected' : '' ?>>
+                            SQL</option>
                     </select>
+                    <?php if (isset($errors['developmentSelect'])) : ?>
+                    <p class="text-danger"><?= $errors['developmentSelect'] ?></p>
+                    <?php endif; ?>
+
                 </div>
+
                 <div class="mb-3 ms-5">
                     <label for="taskSelect" class="form-label">Task Type</label>
-                    <select id="taskSelect" name="taskSelect" class="form-select">
+                    <select id="taskSelect" name="taskSelect" class="form-select" required>
                         <option disabled selected value> -- select an option -- </option>
-                        <option value="Automation">Automation</option>
-                        <option value="Color">Colour</option>
-                        <option value="Font">Font/Text</option>
-                        <option value="Queries">Queries</option>
-                        <option value="CTE">CTEs</option>
-                        <option value="Stored Procedure">Stored Procedures</option>
+                        <option value="Automation"
+                            <?= (isset($_POST['taskSelect']) && $_POST['taskSelect'] == 'Automation') ? 'selected' : '' ?>>
+                            Automation</option>
+                        <option value="Color"
+                            <?= (isset($_POST['taskSelect']) && $_POST['taskSelect'] == 'Color') ? 'selected' : '' ?>>
+                            Colour</option>
+                        <option value="Font"
+                            <?= (isset($_POST['taskSelect']) && $_POST['taskSelect'] == 'Font') ? 'selected' : '' ?>>
+                            Font/Text</option>
+                        <option value="Queries"
+                            <?= (isset($_POST['taskSelect']) && $_POST['taskSelect'] == 'Queries') ? 'selected' : '' ?>>
+                            Queries</option>
+                        <option value="CTE"
+                            <?= (isset($_POST['taskSelect']) && $_POST['taskSelect'] == 'CTE') ? 'selected' : '' ?>>CTEs
+                        </option>
+                        <option value="Stored Procedure"
+                            <?= (isset($_POST['taskSelect']) && $_POST['taskSelect'] == 'Stored Procedure') ? 'selected' : '' ?>>
+                            Stored Procedures</option>
                     </select>
+
+                    <?php if (isset($errors['taskSelect'])) : ?>
+                    <p class="text-danger"><?= $errors['taskSelect'] ?></p>
+                    <?php endif; ?>
+
+
                 </div>
+
             </div>
         </div>
         <div class="d-flex justify-content-center">
             <div class="border w-50 p-3">
                 <div class="mx-5">
-                    <label for="description" class="form-label">Description</label>
-                    <input type="text" class="form-control" name="description" id="description"
-                        placeholder="short description of what the script does">
+
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <input type="text" class="form-control" name="description" id="description"
+                            placeholder="short description of what the script does" required
+                            value="<?= $_POST['description'] ?? '' ?>">
+
+                        <?php if (isset($errors['description'])) : ?>
+                        <p class="text-danger"><?= $errors['description'] ?></p>
+                        <?php endif; ?>
+                    </div>
+
                     <div class="mb-3">
                         <label for="script" class="form-label">Script</label>
                         <textarea type="text" class="form-control" name="script" rows="10" style="height:100%;"
-                            id="script"></textarea>
+                            id="script" required><?= $_POST['script'] ?? '' ?></textarea>
+
+                        <?php if (isset($errors['script'])) : ?>
+                        <p class="text-danger"><?= $errors['script'] ?></p>
+                        <?php endif; ?>
+
                     </div>
                     <div class="d-grid gap-2 d-md-flex justify-content-end">
                         <button type="submit" class="btn btn-primary">Submit</button>
