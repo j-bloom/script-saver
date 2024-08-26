43c6a064f891df104da244969d35a57ae1df44fb
Jaron Bloom <j-bloom7@protonmail.com>
2024-08-26 16:49:00 -0400

    update script view for adding edit/delete functionality

--- views/script.view.php
+++ views/script.view.php
@@ -1,7 +1,10 @@
 <?php require('partials/header.php') ?>
 <?php require('partials/nav.php') ?>
 <div class="p-5">
-    <h1>Scripts</h1>
+    <span class="d-flex align-items-center justify-content-between">
+        <h1>Scripts</h1>
+        <a href="/scripts" class="btn btn-primary">Back to Scripts</a>
+    </span>
 
     <div class="card position-absolute top-50 start-50 translate-middle" style="width: 50rem;">
         <div class="card-body">
@@ -11,7 +14,14 @@
             <p class="card-text">Language: <?= htmlspecialchars($script['language']) ?></p>
             <p class="card-text">Development Type: <?= htmlspecialchars($script['development_type']) ?></p>
             <p class="card-text">Task: <?= htmlspecialchars($script['task_type']) ?></p>
-            <a href="/scripts" class="btn btn-primary">Back to Scripts</a>
+            <div class="d-flex justify-content-end">
+                <form method="POST">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="hidden" name="id" value="<?= $project['id'] ?>">
+                    <a href="/project/edit?id=<?= $project['id'] ?>" class="btn btn-secondary">Edit</a>
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
